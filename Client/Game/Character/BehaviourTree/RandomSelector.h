650e6be575e291e09ef067aa849818359c150347
purohatsu <dnwls010728@gmail.com>
2025-01-04 15:46:12 +0900

    FEAT: RandomSelector 추가

---
+++ Client/Game/Character/BehaviourTree/RandomSelector.h
@@ -0,0 +1,24 @@
+﻿#pragma once
+#include "Selector.h"
+
+namespace BT
+{
+    class RandomSelector : public Selector
+    {
+    public:
+        RandomSelector(const std::wstring& kName);
+        virtual ~RandomSelector() override = default;
+
+        virtual Status Process() override;
+
+        virtual void Reset() override;
+
+    private:
+        const std::vector<std::shared_ptr<Node>>& GetShuffledChildren();
+        
+        const std::vector<std::shared_ptr<Node>>& ShuffleChildren() const;
+
+        std::vector<std::shared_ptr<Node>> shuffled_children_;
+    
+    };
+}
