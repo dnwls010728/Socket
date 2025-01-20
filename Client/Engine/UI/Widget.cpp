dbb02556fd210be10e194bba524d90f9bdee4e01
dnwls010728 <dnwls010728@gmail.com>
2025-01-20 15:33:10 +0900

    FEAT: 신규 UI 시스템 개발 보류

---
+++ Client/Engine/UI/Widget.cpp
@@ -0,0 +1,17 @@
+﻿#include "pch.h"
+#include "Widget.h"
+
+UI::Widget::Widget()
+{
+}
+
+RTTR_REGISTRATION
+{
+    using namespace rttr;
+
+    registration::class_<UI::Widget>("UI::Widget")
+        .constructor<const std::wstring&>()
+        (
+            policy::ctor::as_std_shared_ptr
+        );
+}
---
+++ Client/Engine/UI/Widget.h
@@ -0,0 +1,17 @@
+﻿#pragma once
+#include "Misc/EngineMacros.h"
+#include "rttr/registration_friend.h"
+
+namespace UI
+{
+    class Widget : public std::enable_shared_from_this<Widget>
+    {
+        SHADER_CLASS_HELPER(Widget)
+        GENERATED_BODY(Widget)
+        
+    public:
+        Widget();
+        virtual ~Widget() = default;
+    
+    };
+}
