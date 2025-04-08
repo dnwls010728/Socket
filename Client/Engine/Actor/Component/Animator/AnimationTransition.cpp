51c84269d31ce2ad6a89ad3d958c4689a2f5c801
purohatsu <dnwls010728@gmail.com>
2025-04-08 21:28:58 +0900

    FEAT: AnimatorComponent 개선

---
+++ Client/Engine/Actor/Component/Animator/AnimationTransition.cpp
@@ -0,0 +1,15 @@
+﻿#include "pch.h"
+#include "AnimationTransition.h"
+
+#include "AnimationCondition.h"
+
+AnimationTransition::AnimationTransition(const std::wstring& kTo, const std::shared_ptr<AnimationCondition>& kCondition) :
+    to_(kTo),
+    condition_(kCondition)
+{
+}
+
+bool AnimationTransition::CheckCondition(AnimatorComponent* animator)
+{
+    return condition_->Evaluate(animator);
+}
