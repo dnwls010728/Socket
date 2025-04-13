2a2a2aead1a3e3b0741579abc5fb78e0781377cd
yoonleejun <yoonjjang53@naver.com>
2025-04-13 17:15:02 +0900

    InventoryComponent 피드백 후 수정본

---
+++ Client/Game/Actors/Characters/Player/Components/InventoryComponent.h
@@ -0,0 +1,25 @@
+﻿#pragma once
+#include "Actor/Component/ActorComponent.h"
+#include "Data/PropData.h"
+#include <string>
+
+
+class InventoryComponent:public ActorComponent
+{
+    SHADER_CLASS_HELPER(InventoryComponent)
+    GENERATED_BODY(InventoryComponent,ActorComponent)
+    
+public:
+    InventoryComponent(Actor* owner,const std::wstring&kName);
+    virtual ~InventoryComponent() override=default;
+
+    void SetMaxWeight(float maxWeight);
+    FORCEINLINE bool AddItem(const PropData& propData);
+    FORCEINLINE float GetCurrentWeight() const{return mCurrentWeight;}
+    FORCEINLINE float GetMaxWeight(){return mMaxWeight;}
+
+
+private:
+    float mCurrentWeight;
+    float mMaxWeight;
+};
