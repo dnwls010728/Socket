1e52119c4c4df3c8d164e0848e71cafb7696ffa9
purohatsu <dnwls010728@gmail.com>
2025-05-02 22:49:06 +0900

    FEAT: Controller2DComponent 기능 추가

--- Client/Game/Actors/Characters/Components/Controller2DComponent.cpp
+++ Client/Game/Actors/Characters/Components/Controller2DComponent.cpp
@@ -16,6 +16,8 @@ Controller2DComponent::Controller2DComponent(Actor* owner, const std::wstring& n
     vertical_ray_count_(4),
     horizontal_ray_spacing_(0.f),
     vertical_ray_spacing_(0.f),
+    max_climb_angle_(80.f),
+    max_descend_angle_(80.f),
     collider_(nullptr),
     ray_cast_origins_(),
     collisions_()
@@ -26,7 +28,10 @@ void Controller2DComponent::Move(Math::Vector2 velocity)
 {
     UpdateRayCastOrigins();
     collisions_.Reset();
+    collisions_.velocity_old = velocity;
 
+    if (velocity.y < 0.f) DescendSlope(velocity);
+    
     if (!Math::IsEqual(velocity.x, 0.f)) HorizontalCollisions(velocity);
     if (!Math::IsEqual(velocity.y, 0.f)) VerticalCollisions(velocity);
 
@@ -88,11 +93,39 @@ void Controller2DComponent::HorizontalCollisions(Math::Vector2& velocity)
         
         if (is_hit)
         {
-            velocity.x = (hit_result.distance - kSkinWidth) * direction_x;
-            ray_length = hit_result.distance;
+            float slope_angle = Math::Vector2::Angle(hit_result.normal, Math::Vector2::Up());
+            if (i == 0 && slope_angle <= max_climb_angle_)
+            {
+                if (collisions_.is_descending_slope)
+                {
+                    collisions_.is_descending_slope = false;
+                    velocity = collisions_.velocity_old;
+                }
+                
+                float distance_to_slope_start = 0.f;
+                if (!Math::IsEqual(slope_angle, collisions_.slope_angle_old))
+                {
+                    distance_to_slope_start = hit_result.distance - kSkinWidth;
+                    velocity.x -= distance_to_slope_start * direction_x;
+                }
+                
+                ClimbSlope(velocity, slope_angle);
+                velocity.x += distance_to_slope_start * direction_x;
+            }
 
-            collisions_.is_left = direction_x == -1;
-            collisions_.is_right = direction_x == 1;
+            if (!collisions_.is_climbing_slope || slope_angle > max_climb_angle_)
+            {
+                velocity.x = (hit_result.distance - kSkinWidth) * direction_x;
+                ray_length = hit_result.distance;
+
+                if (collisions_.is_climbing_slope)
+                {
+                    velocity.y = std::tan(collisions_.slope_angle * Math::Deg2Rad()) * Math::Abs(velocity.x);
+                }
+
+                collisions_.is_left = direction_x == -1;
+                collisions_.is_right = direction_x == 1;
+            }
         }
     }
 }
@@ -117,8 +150,82 @@ void Controller2DComponent::VerticalCollisions(Math::Vector2& velocity)
             velocity.y = (hit_result.distance - kSkinWidth) * direction_y;
             ray_length = hit_result.distance;
 
+            if (collisions_.is_climbing_slope)
+            {
+                velocity.x = velocity.y / std::tan(collisions_.slope_angle * Math::Deg2Rad()) * Math::Sign(velocity.x);
+            }
+
             collisions_.is_below = direction_y == -1;
             collisions_.is_above = direction_y == 1;
+        }
+    }
+    
+    if (collisions_.is_climbing_slope)
+    {
+        float direction_x = Math::Sign(velocity.x);
+        ray_length = Math::Abs(velocity.x) + kSkinWidth;
+            
+        Math::Vector2 ray_origin = (direction_x == -1) ? ray_cast_origins_.bottom_left : ray_cast_origins_.bottom_right;
+        ray_origin += Math::Vector2::Up() * velocity.y;
+
+        HitResult hit_result;
+        bool is_hit = Physics2D::RayCast(hit_result, ray_origin, Math::Vector2::Right() * direction_x, ray_length, static_cast<Type::uint16>(ActorLayer::kDefault));
+
+        if (is_hit)
+        {
+            float slope_angle = Math::Vector2::Angle(hit_result.normal, Math::Vector2::Up());
+            if (slope_angle != collisions_.slope_angle)
+            {
+                velocity.x = (hit_result.distance - kSkinWidth) * direction_x;
+                collisions_.slope_angle = slope_angle;
+            }
+        }
+    }
+}
+
+void Controller2DComponent::ClimbSlope(Math::Vector2& velocity, float slope_angle)
+{
+    float move_distance = Math::Abs(velocity.x);
+    float climb_velocity_y = std::sin(slope_angle * Math::Deg2Rad()) * move_distance;
+
+    if (velocity.y <= climb_velocity_y)
+    {
+        velocity.y = climb_velocity_y;
+        velocity.x = std::cos(slope_angle * Math::Deg2Rad()) * move_distance * Math::Sign(velocity.x);
+        collisions_.is_below = true;
+        collisions_.is_climbing_slope = true;
+        collisions_.slope_angle = slope_angle;
+    }
+}
+
+void Controller2DComponent::DescendSlope(Math::Vector2& velocity)
+{
+    float direction_x = Math::Sign(velocity.x);
+    Math::Vector2 ray_origin = (direction_x == -1) ? ray_cast_origins_.bottom_right : ray_cast_origins_.bottom_left;
+    
+    HitResult hit_result;
+    bool is_hit = Physics2D::RayCast(hit_result, ray_origin, Math::Vector2::Down(), std::numeric_limits<float>::max(), static_cast<Type::uint16>(ActorLayer::kDefault));
+
+    if (is_hit)
+    {
+        float slope_angle = Math::Vector2::Angle(hit_result.normal, Math::Vector2::Up());
+        if (!Math::IsEqual(slope_angle, 0.f) && slope_angle <= max_descend_angle_)
+        {
+            if (Math::Sign(hit_result.normal.x) == direction_x)
+            {
+                if (hit_result.distance - kSkinWidth <= std::tan(slope_angle * Math::Deg2Rad()) * Math::Abs(velocity.x))
+                {
+                    float move_distance = Math::Abs(velocity.x);
+                    float descend_velocity_y = std::sin(slope_angle * Math::Deg2Rad()) * move_distance;
+
+                    velocity.x = std::cos(slope_angle * Math::Deg2Rad()) * move_distance * Math::Sign(velocity.x);
+                    velocity.y -= descend_velocity_y;
+
+                    collisions_.slope_angle = slope_angle;
+                    collisions_.is_descending_slope = true;
+                    collisions_.is_below = true;
+                }
+            }
         }
     }
 }
--- Client/Game/Actors/Characters/Components/Controller2DComponent.h
+++ Client/Game/Actors/Characters/Components/Controller2DComponent.h
@@ -21,6 +21,13 @@ public:
         bool is_below;
         bool is_left;
         bool is_right;
+        bool is_climbing_slope;
+        bool is_descending_slope;
+
+        float slope_angle;
+        float slope_angle_old;
+
+        Math::Vector2 velocity_old;
 
         void Reset()
         {
@@ -28,6 +35,11 @@ public:
             is_below = false;
             is_left = false;
             is_right = false;
+            is_climbing_slope = false;
+            is_descending_slope = false;
+
+            slope_angle_old = slope_angle;
+            slope_angle = 0.f;
         }
     };
     
@@ -47,6 +59,8 @@ private:
     void CalculateRaySpacing();
     void HorizontalCollisions(Math::Vector2& velocity);
     void VerticalCollisions(Math::Vector2& velocity);
+    void ClimbSlope(Math::Vector2& velocity, float slope_angle);
+    void DescendSlope(Math::Vector2& velocity);
     
     const float kSkinWidth;
 
@@ -55,6 +69,8 @@ private:
 
     float horizontal_ray_spacing_;
     float vertical_ray_spacing_;
+    float max_climb_angle_;
+    float max_descend_angle_;
 
 #pragma region 컴포넌트
     std::shared_ptr<class ColliderComponent> collider_;
