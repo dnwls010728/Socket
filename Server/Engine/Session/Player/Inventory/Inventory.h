7ad77f55e5877e6b72a79e57a4ebcd529bc10dec
TaeYang112 <xodid4025@yuhan.ac.kr>
2025-05-23 23:31:47 +0900

    FEAT: 누락 파일 추가

---
+++ Server/Engine/Session/Player/Inventory/Inventory.h
@@ -0,0 +1,41 @@
+﻿#pragma once
+#include <map>
+#include <memory>
+
+class Player;
+
+class Inventory
+{
+public:
+    Inventory(Player* owner);
+    ~Inventory() = default;
+    
+    uint32_t GetItemID(uint32_t slot_index) const;
+
+    uint32_t FindItem(uint32_t item_id) const;
+    uint32_t FindFreeSlot() const;
+    uint32_t GetItemCount(uint32_t slot_index) const;
+    uint32_t GetTotalItemCount(uint32_t item_id) const;
+
+    void AddSlot(uint32_t slot_index, uint32_t item_id, uint32_t count);
+    void ChangeCount(uint32_t slot_index, uint32_t count);
+    void Swap(uint32_t first_slot, uint32_t second_slot);
+    void Remove(uint32_t slot_index);
+    
+    void Update();
+
+private:
+    struct Slot
+    {
+        uint32_t item_id;
+        uint32_t count;
+    };
+
+    Player* player_;
+    
+    std::map<uint32_t, Slot> slots_;
+
+public:
+    inline const std::map<uint32_t, Slot>& GetSlots() const { return slots_; }
+    
+};
