23d85546dc8061bd9792e18fe8c6e70d11bcd955
purohatsu <dnwls010728@gmail.com>
2025-05-23 22:02:11 +0900

    FEAT: 임시 커밋

--- Server/Map/MapObject.h
+++ Server/Map/MapObject.h
@@ -8,6 +8,12 @@ class MapObject
 public:
     MapObject();
     virtual ~MapObject() = default;
+    
+    inline void SetObjectId(uint32_t object_id) { object_id_ = object_id; }
+    inline uint32_t GetObjectId() const { return object_id_; }
+
+    inline void SetPosition(const Math::Vector2& position) { position_ = position; }
+    inline const Math::Vector2& GetPosition() const { return position_; }
 
 private:
     uint32_t object_id_;
