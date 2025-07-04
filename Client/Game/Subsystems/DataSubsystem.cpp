d7bb66eb657dce5f41d07b3fa43c6838271df818
purohatsu <dnwls010728@gmail.com>
2025-07-04 17:49:05 +0900

    FEAT: Data 서브 시스템 추가

---
+++ Client/Game/Subsystems/DataSubsystem.cpp
@@ -0,0 +1,22 @@
+﻿#include "pch.h"
+#include "DataSubsystem.h"
+
+DataSubsystem::DataSubsystem()
+{
+}
+
+void DataSubsystem::Init()
+{
+    GameInstanceSubsystem::Init();
+}
+
+RTTR_REGISTRATION
+{
+    using namespace rttr;
+
+    registration::class_<DataSubsystem>("DataSubsystem")
+        .constructor<>()
+        (
+            policy::ctor::as_raw_ptr
+        );
+}
---
+++ Client/Game/Subsystems/DataSubsystem.h
@@ -0,0 +1,58 @@
+﻿#pragma once
+#include "Misc/StringHelper.h"
+#include "Subsystem/GameInstanceSubsystem.h"
+
+#include "yaml-cpp/yaml.h"
+
+struct MobStats
+{
+    int32_t lv;
+    float hp;
+    float speed;
+};
+
+struct MobData
+{
+    MobStats stats;
+    std::wstring animation_pack;
+};
+
+namespace YAML
+{
+    template<>
+    struct convert<MobStats>
+    {
+        static bool decode(const Node& node, MobStats& data)
+        {
+            if (!node.IsMap()) return false;
+            data.lv = node["lv"].as<int32_t>(0);
+            data.hp = node["hp"].as<float>(0.f);
+            data.speed = node["speed"].as<float>(0.f);
+            return true;
+        }
+    };
+    
+    template<>
+    struct convert<MobData>
+    {
+        static bool decode(const Node& node, MobData& data)
+        {
+            if (!node.IsMap()) return false;
+            data.stats = node["stats"].as<MobStats>();
+            data.animation_pack = StringHelper::UTF8ToUTF16(node["animation_pack"].as<std::string>(""));
+            return true;
+        }
+    };
+}
+
+class DataSubsystem : public GameInstanceSubsystem
+{
+    GENERATED_BODY(DataSubsystem, GameInstanceSubsystem)
+    
+public:
+    DataSubsystem();
+    virtual ~DataSubsystem() override = default;
+
+    virtual void Init() override;
+    
+};
