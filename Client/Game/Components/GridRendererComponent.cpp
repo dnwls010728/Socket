f9daaaf944414144fc37babb2477692510090864
Choi Woojin <38973547+dnwls010728@users.noreply.github.com>
2025-09-01 00:41:27 +0900

    FEAT: GridrendererComponent 추가

---
+++ Client/Game/Components/GridRendererComponent.cpp
@@ -0,0 +1,113 @@
+﻿#include "pch.h"
+#include "GridRendererComponent.h"
+
+#include "Windows/DX/Shape.h"
+
+GridRendererComponent::GridRendererComponent(Actor* owner, const std::wstring& name) :
+    ActorComponent(owner, name),
+    vertices_(),
+    indices_(),
+    shape_(nullptr),
+    color_(Math::Color::White),
+    rows_(0),
+    cols_(0)
+{
+}
+
+void GridRendererComponent::InitializeComponent()
+{
+    ActorComponent::InitializeComponent();
+
+    shape_ = std::make_shared<Shape>();
+    shape_->SetPrimitiveTopology(D3D11_PRIMITIVE_TOPOLOGY_LINELIST);
+    shape_->SetColor(color_);
+    shape_->SetZOrder(std::numeric_limits<int32_t>::max());
+    
+    Refresh();
+}
+
+void GridRendererComponent::UninitializeComponent()
+{
+    ActorComponent::UninitializeComponent();
+    
+    World::Get()->RemoveShape(shape_);
+}
+
+void GridRendererComponent::OnEnable()
+{
+    ActorComponent::OnEnable();
+
+    World::Get()->AddShape(shape_);
+}
+
+void GridRendererComponent::OnDisable()
+{
+    ActorComponent::OnDisable();
+
+    World::Get()->RemoveShape(shape_);
+}
+
+void GridRendererComponent::Refresh()
+{
+    vertices_.clear();
+    indices_.clear();
+
+    for (uint32_t row = 0; row <= rows_; ++row)
+    {
+        float py = static_cast<float>(row);
+        
+        DefaultVertex v0 = {
+            {0.f, py, 0.f}, {1.f, 1.f, 1.f, 1.f}
+        };
+
+        DefaultVertex v1 = {
+            {static_cast<float>(cols_), py, 0.f}, {1.f, 1.f, 1.f, 1.f}
+        };
+
+        uint32_t base = static_cast<uint32_t>(vertices_.size());
+
+        vertices_.push_back(v0);
+        vertices_.push_back(v1);
+        
+        indices_.push_back(base);
+        indices_.push_back(base + 1);
+    }
+
+    for (uint32_t col = 0; col <= cols_; ++col)
+    {
+        float px = static_cast<float>(col);
+
+        DefaultVertex v0 = {
+            {px, 0.f, 0.f}, {1.f, 1.f, 1.f, 1.f}
+        };
+
+        DefaultVertex v1 = {
+            {px, static_cast<float>(rows_), 0.f}, {1.f, 1.f, 1.f, 1.f}
+        };
+
+        uint32_t base = static_cast<uint32_t>(vertices_.size());
+
+        vertices_.push_back(v0);
+        vertices_.push_back(v1);
+        
+        indices_.push_back(base);
+        indices_.push_back(base + 1);
+    }
+
+    if (shape_)
+    {
+        shape_->SetVertices(vertices_);
+        shape_->SetIndices(indices_);
+    }
+}
+
+RTTR_REGISTRATION
+{
+    using namespace rttr;
+
+    registration::class_<GridRendererComponent>("GridRendererComponent")
+        .constructor<Actor*, const std::wstring&>()
+        (
+            policy::ctor::as_std_shared_ptr
+        );
+}
---
+++ Client/Game/Components/GridRendererComponent.h
@@ -0,0 +1,38 @@
+﻿#pragma once
+#include "Math/Color.h"
+
+class GridRendererComponent : public ActorComponent
+{
+    SHADER_CLASS_HELPER(GridRendererComponent)
+    GENERATED_BODY(GridRendererComponent, ActorComponent)
+    
+public:
+    GridRendererComponent(Actor* owner, const std::wstring& name);
+    virtual ~GridRendererComponent() override = default;
+    
+    FORCEINLINE uint32_t GetRows() const { return rows_; }
+    FORCEINLINE uint32_t GetCols() const { return cols_; }
+
+    FORCEINLINE void SetColor(const Math::Color& color) { color_ = color; }
+    FORCEINLINE const Math::Color& GetColor() const { return color_; }
+
+protected:
+    virtual void InitializeComponent() override;
+    virtual void UninitializeComponent() override;
+    virtual void OnEnable() override;
+    virtual void OnDisable() override;
+
+private:
+    void Refresh();
+    
+    std::vector<DefaultVertex> vertices_;
+    std::vector<uint32_t> indices_;
+
+    std::shared_ptr<Shape> shape_;
+    
+    Math::Color color_;
+
+    uint32_t rows_;
+    uint32_t cols_;
+    
+};
